6faeb4cbe3bca537980243642f56c4838b6e5c1a
Vũ Thiện Khôi <khoi2mai.studio@gmail.com>
2026-06-30 16:58:55 +0700

    update

--- manager/app/src/main/java/com/bunny/su/ui/screen/about/AboutMaterial.kt
+++ manager/app/src/main/java/com/bunny/su/ui/screen/about/AboutMaterial.kt
@@ -39,6 +39,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
